8c221a2e8f82b78a6ce66af4f8983875723b13ad
tdgmcode <tdgmdev@gmail.com>
2023-03-15 02:09:04 -0400

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,13 @@
 import adapter from '@sveltejs/adapter-node'
+import preprocess from 'svelte-preprocess';3
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: [
+    preprocess({
+        postcss: true
+    })
+  ],
   kit: {
     adapter: adapter(), 
   }
